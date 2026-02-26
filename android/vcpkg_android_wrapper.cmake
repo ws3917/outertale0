811d3094f9622415aa25e5e4107bcbf5a41e0d90
ws3917 <ws3917@outlook.com>
2026-02-26 11:23:00 +0800

    no more android

--- android/vcpkg_android_wrapper.cmake
+++
@@ -1,8 +0,0 @@
-if(ANDROID_ABI STREQUAL "arm64-v8a")
-    set(VCPKG_TARGET_TRIPLET "custom-arm64-android-dynamic" CACHE STRING "")
-elseif(ANDROID_ABI STREQUAL "x86_64")
-    set(VCPKG_TARGET_TRIPLET "custom-x64-android-dynamic" CACHE STRING "")
-endif()
-
-set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE "$ENV{ANDROID_NDK_HOME}/build/cmake/android.toolchain.cmake" CACHE FILEPATH "")
-include("$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake")
--- src/scene/S_Init.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../type/Scene.hpp"
-
-class S_Init : public Scene {
- public:
-  S_Init();
-  void enter() override;
-};
